5d6c48ac46bc6a2f544208a31d1dba6280df21fd
ImRayh <soImRayh>
2022-05-10 14:49:05 -0300

    composer added

--- src/main/java/com/ifg/spring/controllers/IndexController.java
+++ src/main/java/com/ifg/spring/controllers/IndexController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("")
     public String index(){
-        return "index";
+        return "Index";
     }
 }
