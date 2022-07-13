ef44c1e1e777ebbe7ef6feefb7f9040098e326c2
ImRayh <soImRayh>
2022-07-13 07:46:44 -0300

    add .env

--- src/main/java/com/ifg/spring/Application.java
+++ src/main/java/com/ifg/spring/Application.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
